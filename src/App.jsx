ee09d7c6717603df6ec1e9bb3c03beaea737c7eb
unknown <archanabhairi2002@gmail.com>
2025-09-02 13:07:07 +0530

    changed wopi url

--- src/App.jsx
+++ src/App.jsx
@@ -32,10 +32,11 @@ const App = () => {
       return;
     }
 
-    const wopiSrc = `${locationOrigin}/wopi/files/1`;
+    // const wopiSrc = `${locationOrigin}/wopi/files/1`;
+    const wopiSrc = 'https://collabora-backend.b2yinfy.com/wopi/files/sample.docx'
     console.log(`wopiSrc: ${wopiSrc}`);
 
-    fetch(`/collaboraUrl?server=${wopiClientHost}`)
+    fetch(`https://collabora-backend.b2yinfy.com/collaboraUrl?server=${wopiClientHost}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
